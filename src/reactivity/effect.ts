82245cdc39e1f57b6bda35bc4686e7ac4c78d81f
bluehyssopu <1767807919@qq.com>
2022-07-10 10:32:13 +0800

    实现effect.stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,9 @@
+import { extend } from '../shared';
 class ReactiveEffect {
   private _fn: any;
-
+  active = true;
+  deps = [];
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -9,6 +12,21 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if(this.active) {
+      cleanupEffect(this);
+      if(this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new Map();
@@ -17,27 +35,33 @@ export function track(target, key) {
   // target -> key -> dep
   let depsMap = targetMap.get(target);
   // 解决初始化的问题
-  if(!depsMap) {
+  if (!depsMap) {
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
 
   let dep = depsMap.get(key);
-  if(!dep) {
+  if (!dep) {
     dep = new Set();
-    depsMap.set(key, dep)
+    depsMap.set(key, dep);
   }
-  
+
+  // activeEffect可能会出现undefined 需要提前处理
+  if(!activeEffect) return;
+
   dep.add(activeEffect);
   // const dep = new Set();
+
+  // 反向收集deps
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
 
-  for(const effect of dep) {
-    if(effect.scheduler) {
+  for (const effect of dep) {
+    if (effect.scheduler) {
       effect.scheduler();
     } else {
       effect.run();
@@ -46,10 +70,21 @@ export function trigger(target, key) {
 }
 
 let activeEffect;
-export function effect(fn, options:any = {}) {
+export function effect(fn, options: any = {}) {
   // fn
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  // options
+  Object.assign(_effect, options);
+  // extends
+  extend(_effect, options);
+
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
