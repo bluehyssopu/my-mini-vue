81554c66af488f4d5286ae1f86ab9d43483a607c
bluehyssopu <1767807919@qq.com>
2022-08-04 14:53:07 +0800

    补充isReactive的测试

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -6,8 +6,11 @@ describe("readonly", () => {
     const original = { foo: 1, bar: { baz: 2 } };
     const wrapped = readonly(original);
     expect(wrapped).not.toBe(original);
-    // expect(isReadonly(wrapped)).toBe(true);
+    expect(isReadonly(wrapped)).toBe(true);
     expect(isReadonly(original)).toBe(false);
+    expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isReadonly(original.bar)).toBe(false);
+
     expect(wrapped.foo).toBe(1);
   });
 
