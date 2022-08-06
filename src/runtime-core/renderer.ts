2a97a7935f66ab761cdc5d1e059f98b578337d3f
bluehyssopu <1767807919@qq.com>
2022-08-06 21:47:10 +0800

    实现component主流程初始化

---
+++ src/runtime-core/renderer.ts
@@ -0,0 +1,31 @@
+import { createComponentInstence, setupComponent } from "./component";
+
+export function render(vnode, container) {
+  // patch
+  patch(vnode, container);
+}
+
+function patch(vnode, container) {
+  // 去处理组件
+  processComponent(vnode, container);
+}
+
+function processComponent(vnode: any, container: any) {
+  mountComponent(vnode, container);
+}
+
+function mountComponent(vnode: any, container) {
+  const instance = createComponentInstence(vnode);
+
+  setupComponent(instance);
+  setupRenderEffect(instance, container);
+}
+
+function setupRenderEffect(instance: any, container) {
+  const subTree = instance.render();
+
+  // vnode -> patch
+  // vnode -> element -> mountElement
+
+  patch(subTree, container);
+}
